d8d06e8324ed647490e223bd13904e6a5dba7c68
JendareyTechnologies <ak@jendareyconsulting.com>
2023-09-23 16:27:53 +0000

    my first commit

---
+++ src/main/java/com/jendareytech/devopsenginersinfo/DevopsenginersinfoApplication.java
@@ -0,0 +1,13 @@
+package com.jendareytech.devopsenginersinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsenginersinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsenginersinfoApplication.class, args);
+	}
+
+}
